d7b3a2ed3b9d9ca0aeb200102ff78070bb31d767
Magdiel Palacios <magefra9@hotmail.com>
2020-07-20 11:26:03 -0500

    Agregando seguridad con JWT

--- SocialMedia.Api/Startup.cs
+++ SocialMedia.Api/Startup.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -7,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using SocialMedia.Core.src.CustomEntities;
 using SocialMedia.Core.src.Interfaces.Repositories;
@@ -21,6 +23,7 @@ using SocialMedia.Infrastructure.src.Services;
 using System;
 using System.IO;
 using System.Reflection;
+using System.Text;
 
 namespace SocialMedia.Api
 {
@@ -97,6 +100,26 @@ namespace SocialMedia.Api
             });
 
 
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Authentication:Issuer"],
+                    ValidAudience = Configuration["Authentication:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Authentication:SecretKey"]))
+                };
+            });
+
+
             services.AddMvc(options =>
             {
                 options.Filters.Add<ValidationFilter>();
@@ -130,7 +153,12 @@ namespace SocialMedia.Api
 
             app.UseRouting();
 
+
+            app.UseAuthentication();
+
             app.UseAuthorization();
+
+         
 
             app.UseEndpoints(endpoints =>
             {
